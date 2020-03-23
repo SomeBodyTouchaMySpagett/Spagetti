dc28287629a9f166dbcd3d5b1496c47f4a3f0dc9
Spagett <tk42356@gmail.com>
2020-03-23 04:26:04 +0300

    1
    Script1, Script2

---
+++ Scripts/Script1/Action.c
@@ -0,0 +1,172 @@
+Action()
+{
+
+	web_set_sockets_option("SSL_VERSION", "2&3");
+
+	/*Possible OAUTH authorization was detected. It is recommended to correlate the authorization parameters.*/
+
+	web_url("WebTours", 
+		"URL=http://localhost:1080/WebTours/", 
+		"TargetFrame=", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=", 
+		"Snapshot=t98.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	/* Login */
+
+	lr_think_time(7);
+
+	lr_start_transaction("1_transaction_Login");
+
+	lr_think_time(8);
+
+	web_submit_data("login.pl", 
+		"Action=http://localhost:1080/cgi-bin/login.pl", 
+		"Method=POST", 
+		"TargetFrame=", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?in=home", 
+		"Snapshot=t114.inf", 
+		"Mode=HTML", 
+		ITEMDATA, 
+		"Name=userSession", "Value=128385.84236533zzADitzpHzzzzzzHDQzcDpAiAz", ENDITEM, 
+		"Name=username", "Value=jojo", ENDITEM, 
+		"Name=password", "Value=bean", ENDITEM, 
+		"Name=login.x", "Value=73", ENDITEM, 
+		"Name=login.y", "Value=7", ENDITEM, 
+		"Name=JSFormSubmit", "Value=off", ENDITEM, 
+		LAST);
+
+	lr_end_transaction("1_transaction_Login",LR_AUTO);
+
+	lr_start_transaction("2_transaction_Flights");
+
+	lr_think_time(18);
+
+	web_url("welcome.pl", 
+		"URL=http://localhost:1080/cgi-bin/welcome.pl?page=search", 
+		"TargetFrame=", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?page=menu&in=home", 
+		"Snapshot=t116.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	lr_end_transaction("2_transaction_Flights",LR_AUTO);
+
+	lr_start_transaction("3_transaction_Flight_Info");
+
+	lr_think_time(35);
+
+	web_submit_data("reservations.pl", 
+		"Action=http://localhost:1080/cgi-bin/reservations.pl", 
+		"Method=POST", 
+		"TargetFrame=", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/reservations.pl?page=welcome", 
+		"Snapshot=t118.inf", 
+		"Mode=HTML", 
+		ITEMDATA, 
+		"Name=advanceDiscount", "Value=0", ENDITEM, 
+		"Name=depart", "Value=London", ENDITEM, 
+		"Name=departDate", "Value=03/24/2020", ENDITEM, 
+		"Name=arrive", "Value=Paris", ENDITEM, 
+		"Name=returnDate", "Value=03/28/2020", ENDITEM, 
+		"Name=numPassengers", "Value=1", ENDITEM, 
+		"Name=seatPref", "Value=Window", ENDITEM, 
+		"Name=seatType", "Value=Business", ENDITEM, 
+		"Name=findFlights.x", "Value=70", ENDITEM, 
+		"Name=findFlights.y", "Value=5", ENDITEM, 
+		"Name=.cgifields", "Value=roundtrip", ENDITEM, 
+		"Name=.cgifields", "Value=seatType", ENDITEM, 
+		"Name=.cgifields", "Value=seatPref", ENDITEM, 
+		LAST);
+
+	lr_end_transaction("3_transaction_Flight_Info",LR_AUTO);
+
+	lr_think_time(10);
+
+	lr_start_transaction("4_transaction_Select_Time");
+
+	lr_think_time(13);
+
+	web_submit_data("reservations.pl_2", 
+		"Action=http://localhost:1080/cgi-bin/reservations.pl", 
+		"Method=POST", 
+		"TargetFrame=", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/reservations.pl", 
+		"Snapshot=t120.inf", 
+		"Mode=HTML", 
+		ITEMDATA, 
+		"Name=outboundFlight", "Value=242;149;03/24/2020", ENDITEM, 
+		"Name=numPassengers", "Value=1", ENDITEM, 
+		"Name=advanceDiscount", "Value=0", ENDITEM, 
+		"Name=seatType", "Value=Business", ENDITEM, 
+		"Name=seatPref", "Value=Window", ENDITEM, 
+		"Name=reserveFlights.x", "Value=45", ENDITEM, 
+		"Name=reserveFlights.y", "Value=9", ENDITEM, 
+		LAST);
+
+	lr_end_transaction("4_transaction_Select_Time",LR_AUTO);
+
+	lr_start_transaction("5_transaction_Payment");
+
+	lr_think_time(20);
+
+	lr_think_time(13);
+
+	web_submit_data("reservations.pl_3", 
+		"Action=http://localhost:1080/cgi-bin/reservations.pl", 
+		"Method=POST", 
+		"TargetFrame=", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/reservations.pl", 
+		"Snapshot=t123.inf", 
+		"Mode=HTML", 
+		ITEMDATA, 
+		"Name=firstName", "Value=Jojo", ENDITEM, 
+		"Name=lastName", "Value=Bean", ENDITEM, 
+		"Name=address1", "Value=Sting", ENDITEM, 
+		"Name=address2", "Value=London", ENDITEM, 
+		"Name=pass1", "Value=Jojo Bean", ENDITEM, 
+		"Name=creditCard", "Value=123456789", ENDITEM, 
+		"Name=expDate", "Value=11/21", ENDITEM, 
+		"Name=saveCC", "Value=on", ENDITEM, 
+		"Name=oldCCOption", "Value=on", ENDITEM, 
+		"Name=numPassengers", "Value=1", ENDITEM, 
+		"Name=seatType", "Value=Business", ENDITEM, 
+		"Name=seatPref", "Value=Window", ENDITEM, 
+		"Name=outboundFlight", "Value=242;149;03/24/2020", ENDITEM, 
+		"Name=advanceDiscount", "Value=0", ENDITEM, 
+		"Name=returnFlight", "Value=", ENDITEM, 
+		"Name=JSFormSubmit", "Value=off", ENDITEM, 
+		"Name=buyFlights.x", "Value=64", ENDITEM, 
+		"Name=buyFlights.y", "Value=5", ENDITEM, 
+		"Name=.cgifields", "Value=saveCC", ENDITEM, 
+		LAST);
+
+	lr_end_transaction("5_transaction_Payment",LR_AUTO);
+
+	lr_start_transaction("6_transaction_Logoff");
+
+	lr_think_time(28);
+
+	web_url("welcome.pl_2", 
+		"URL=http://localhost:1080/cgi-bin/welcome.pl?signOff=1", 
+		"TargetFrame=", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?page=menu&in=flights", 
+		"Snapshot=t124.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	lr_end_transaction("6_transaction_Logoff",LR_AUTO);
+
+	return 0;
+}
